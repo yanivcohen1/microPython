390ee243d5b444d54f9d5edb6877d25e9210abf8
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-06-30 09:47:00 +0300

    ud

--- MicroWebSrv/workSpace/ws.py
+++ MicroWebSrv/workSpace/ws.py
@@ -2,7 +2,8 @@ import os
 try:
     os.remove("machine.py")
 except :
-    print ("machine not exist")
+    pass
+    # print ("machine not exist")
 print (os.listdir())
 import ynet
 import main_start_ws
