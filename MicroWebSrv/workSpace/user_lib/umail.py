1b5d054e03ced8c7c8963e32ce0b5c0c7ea44408
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-17 19:58:53 +0300

    update

--- MicroWebSrv/workSpace/user_lib/umail.py
+++ MicroWebSrv/workSpace/user_lib/umail.py
@@ -4,9 +4,16 @@
 
 # How to use it - tested is't working
 # import user_lib.umail as umail
-# smtp = umail.SMTP('smtp.gmail.com', 587, username='yanco54321@gmail.com', password='thePassword')
+# smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True) # Gmail's SSL port
+# smtp.login('yanco54321@gmail.com', 'thePassword')
 # smtp.to('yaniv.choen.1@gmail.com')
-# smtp.send("This is an example.")
+# smtp.write("From: yan <yanco54321@gmail.com>\n")
+# smtp.write("To: Alice <yaniv.choen.1@gmail.com>\n")
+# smtp.write("Subject: this is a test subject\n\n")
+# smtp.write("first line test.\n")
+# smtp.write("secend line test.\n")
+# smtp.write("...\n")
+# smtp.send()
 # smtp.quit()
 
 import socket as usocket
