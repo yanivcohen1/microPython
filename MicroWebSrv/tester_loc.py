c15afeb4a2ff712c0419b0f0fb7cfad4c747750c
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-18 14:44:49 +0300

    ud

--- MicroWebSrv/tester_loc.py
+++ MicroWebSrv/tester_loc.py
@@ -1,7 +1,7 @@
-import json
+import binascii
 
-send = {}
-send['first'] = 'yan'
-send['last'] = 'cohen'
-send['last'] = 'cohe1'
-print(send)
+bin_res = bin(int(binascii.hexlify(b'hello'), 16))
+print(bin_res) # '0b110100001100101011011000110110001101111'
+n = int(bin_res, 2)
+str_res = binascii.unhexlify('%x' % n).decode("utf-8")
+print(str_res)
