44dd7c1a713eda69484d6a6b1feaab3406f4b463
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-18 17:39:34 +0300

    update

--- MicroWebSrv/workSpace/user_lib/ynet.py
+++ MicroWebSrv/workSpace/user_lib/ynet.py
@@ -1,12 +1,12 @@
 
 import network
-import encryption
+from user_lib.encryption import dencrypt
 
 sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect("HOTBOX-89BA-yaniv", encryption.dencrypt('0x30353238373238353434'))
+        sta_if.connect("HOTBOX-89BA-yaniv", dencrypt('0x30353238373238353434'))
         while not sta_if.isconnected():
             pass
 
