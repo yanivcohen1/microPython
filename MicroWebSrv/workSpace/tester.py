0453ca0e01073c3a0e2097ab66b20692916a15a4
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-18 15:24:05 +0300

    ud

--- MicroWebSrv/workSpace/tester.py
+++ MicroWebSrv/workSpace/tester.py
@@ -1,15 +1,10 @@
-# https://github.com/shawwwn/uMail
+from machine import Pin, I2C
+from user_lib import SSD1315_OLED_DISP
 
-import user_lib.umail as umail
+# using default address 0x3C
+i2c = I2C(sda=Pin(21), scl=Pin(22))
+display = SSD1315_OLED_DISP.SSD1306_I2C(128, 64, i2c)
 
-smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True) # Gmail's SSL port
-smtp.login('yanco54321@gmail.com', 'thePassword')
-smtp.to('yaniv.choen.1@gmail.com')
-smtp.write("From: yan <yanco54321@gmail.com>\n")
-smtp.write("To: Alice <yaniv.choen.1@gmail.com>\n")
-smtp.write("Subject: this is a test subject\n\n")
-smtp.write("first line test.\n")
-smtp.write("secend line test.\n")
-smtp.write("...\n")
-smtp.send()
-smtp.quit()
+# Print Hello World on the first line:
+display.text('Hello, World!', 0, 0, 1)
+display.show()
