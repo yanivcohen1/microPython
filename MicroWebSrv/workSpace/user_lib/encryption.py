fe3802e1224f869f2d47d4822fbc4a7afc836189
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-19 13:29:07 +0300

    ud

--- MicroWebSrv/workSpace/user_lib/encryption.py
+++ MicroWebSrv/workSpace/user_lib/encryption.py
@@ -6,9 +6,9 @@ def encrypt(str):
     encrypt_res = enc.encrypt(data_bytes + b'\x00' * ((16 - (len(data_bytes) % 16)) % 16))
     return encrypt_res
 
-def decrypt(encrypt_str):
+def decrypt(encrypt_bytes_res): # b'\xfe!F\x87?\xdb\x19\x18\xcdM\x83\x9b\xaa\x02\xa9\x04'
     dec = ucryptolib.aes(b'1234567890123456', 1)
-    decrypt_res = dec.decrypt(encrypt_str) # b'input plaintext\x00'
+    decrypt_res = dec.decrypt(encrypt_bytes_res)
     str_res = decrypt_res.decode("utf-8")
     return str_res
 
