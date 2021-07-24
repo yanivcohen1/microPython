c21a896f51746da56f2d71af638a00274e92a847
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-24 16:10:18 +0300

    ud

--- MicroWebSrv/workSpace/user_lib/mqtt.py
+++ MicroWebSrv/workSpace/user_lib/mqtt.py
@@ -76,6 +76,7 @@ while True:
 #     unique_id: "vegepod_level"
 #     value_template: "{{ value_json.level}}"
 
+# for binary_sensor only sending 'ON' or 'OFF'
 # binary_sensor:
 #   - platform: mqtt
 #     state_topic: "homeassistant/binary_sensor/vegepod/state"
