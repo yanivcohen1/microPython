1fb614a74c0cec565cf02ca3ef3341e4c1087f58
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-25 17:24:39 +0300

    ud

--- MicroWebSrv/workSpace/user_lib/Stepper_uln2003.py
+++ MicroWebSrv/workSpace/user_lib/Stepper_uln2003.py
@@ -58,3 +58,12 @@ class Stepper:
 
 def create(pin1, pin2, pin3, pin4, delay=2, mode='HALF_STEP'):
 	return Stepper(mode, pin1, pin2, pin3, pin4, delay)
+
+# use it
+# import Stepper
+# from machine import Pin
+# s1 = Stepper.create(Pin(16,Pin.OUT),Pin(17,Pin.OUT),Pin(5,Pin.OUT),Pin(18,Pin.OUT), delay=2)
+# s1.step(100)
+# s1.step(100,-1)
+# s1.angle(180)
+# s1.angle(360,-1)
