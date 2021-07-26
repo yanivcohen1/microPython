3ce7e5b60b17bf4c89db4f827bd93ed430f7aa67
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-26 17:26:35 +0300

    add copy

--- MicroWebSrv/workSpace/tester.py
+++ MicroWebSrv/workSpace/tester.py
@@ -1,37 +1,13 @@
-# micro servo PWM freq=50, dutycycle[30=0', 140=180']
-from machine import Pin, ADC, PWM
-from time import sleep
+from user_lib.copy import deepcopy
 
-servo = PWM(Pin(4), freq=50) # 50HZ = 20ms cycle
-SERVO_MAX_ANGLE = 180
-SERVO_MIN_ANGLE = 0
-SERVO_MAX_DUTY = 140 # angle=180'
-SERVO_MIN_DUTY = 30 # angle=0'
-SERVO_RANG_DUTY = SERVO_MAX_DUTY - SERVO_MIN_DUTY
-POT_MAX_READ = 4095
-sliderPot = ADC(Pin(34))
-sliderPot.atten(ADC.ATTN_11DB) # Full range: 3.3v
-lastDuty = SERVO_MIN_DUTY # init angle=0'
-init = True
-try:
-    while True:
-        current_sliderPot = sliderPot.read() # min is 0, max read 4095
-        # print(current_sliderPot)
-        calcPotDuty = int(SERVO_MIN_DUTY + SERVO_RANG_DUTY * current_sliderPot / POT_MAX_READ)
-        if not(calcPotDuty == lastDuty or calcPotDuty == lastDuty + 1  \
-            or calcPotDuty == lastDuty - 1):
-            if init: 
-                servo.duty(SERVO_MIN_DUTY)
-                print('init is:', servo.duty())
-                init = False
-                print('angle is: 0')
-            else:
-                servo.duty(calcPotDuty)
-                print('angle is:', current_sliderPot * SERVO_MAX_ANGLE / POT_MAX_READ)
-            lastDuty = calcPotDuty
-            sleep(15/1000) # 15ms time take the motor to get to position
-        else: sleep(1/1000) # 1ms loop delay
-except KeyboardInterrupt : # control+C press
-    pass
-servo.deinit()
+old_list = [[1, 2, 3], [4, 'a', 5]]
+new_list = old_list
 
+new_list = deepcopy(old_list)
+new_list[1][2] = 'b'
+
+print('Old List:', old_list)
+print('ID of Old List:', id(old_list))
+
+print('New List:', new_list)
+print('ID of New List:', id(new_list))
