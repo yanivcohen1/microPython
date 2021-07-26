56022f701627a14f824673941b4031ad638dce79
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-26 18:27:40 +0300

    update

--- MicroWebSrv/workSpace/user_lib/encryption.py
+++ MicroWebSrv/workSpace/user_lib/encryption.py
@@ -1,14 +1,15 @@
 import ucryptolib
 import uhashlib
 
+SECRET_KEY = b'1234567890123456'
 def encrypt(str):
-    enc = ucryptolib.aes(b'1234567890123456', 1)
+    enc = ucryptolib.aes(SECRET_KEY, 1)
     data_bytes = str.encode("utf-8")
     encrypt_res = enc.encrypt(data_bytes + b'\x00' * ((16 - (len(data_bytes) % 16)) % 16))
     return encrypt_res
 
 def decrypt(encrypt_bytes_res): # b'\xfe!F\x87?\xdb\x19\x18\xcdM\x83\x9b\xaa\x02\xa9\x04'
-    dec = ucryptolib.aes(b'1234567890123456', 1)
+    dec = ucryptolib.aes(SECRET_KEY, 1)
     decrypt_res = dec.decrypt(encrypt_bytes_res)
     str_res = decrypt_res.decode("utf-8")
     return str_res
