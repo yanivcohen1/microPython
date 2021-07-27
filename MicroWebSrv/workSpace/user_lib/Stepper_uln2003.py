cf35faf9daeb6b4c4fcf3e72e78898e03d4453f7
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-27 14:25:08 +0300

    update

--- MicroWebSrv/workSpace/user_lib/Stepper_uln2003.py
+++ MicroWebSrv/workSpace/user_lib/Stepper_uln2003.py
@@ -6,10 +6,10 @@ import time
 class Stepper:
     # http://www.jangeox.be/2013/10/stepper-motor-28byj-48_25.html
     # the ratio between input and ouput from the geer is 1 to 64 and for one input spin in half step is 8
-    # so number of steps per one circle is = 1*8*64= 512 in HALF_STEPs
+    # so number of steps per one circle is = 1*8*64= 512 in 8 HALF_STEPs
     # also from the spec 5.625/64 angle for one step, you need 8 for one internal cycle in HALF_STEP
-    # so for 360' - 360*64/(5.625*8)=512 in HALF_STEPs
-    FULL_ROTATION = int(4075.7728395061727 / 8) # number of steps for full rotation in half_step
+    # so for 360' - 360*64/(5.625*8)=512 in 8 HALF_STEPs
+    FULL_ROTATION = int(4075.7728395061727 / 8) # 512 number of steps for full rotation in half_step
 
     HALF_STEP = [
         [0, 0, 0, 1],
@@ -67,8 +67,8 @@ def create(pin1, pin2, pin3, pin4, delay=2, mode='HALF_STEP'):
 # use it
 # import Stepper
 # from machine import Pin
-# s1 = Stepper.create(Pin(16,Pin.OUT),Pin(17,Pin.OUT),Pin(5,Pin.OUT),Pin(18,Pin.OUT), delay=2)
-# s1.step(100)
-# s1.step(100,-1)
-# s1.angle(180)
-# s1.angle(360,-1)
+# s1 = Stepper.create(Pin(16,Pin.OUT),Pin(17,Pin.OUT),Pin(5,Pin.OUT),Pin(18,Pin.OUT), delay=2) # defoult is HALF_STEP 
+# s1.step(100) # 100 steps of 8 HALF_STEPs eche step
+# s1.step(100,-1) # backwards 100 steps of 8 HALF_STEPs eche step
+# s1.angle(180) # forwards 180'
+# s1.angle(360,-1) # backwards 360'
