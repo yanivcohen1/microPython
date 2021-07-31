e162173d18d83fde601f5208775125e7d5a6e79e
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-31 18:35:16 +0300

    update

--- MicroWebSrv/workSpace/test.py
+++ MicroWebSrv/workSpace/test.py
@@ -1,4 +1,10 @@
-from user_lib.servo import tester
-from user_lib.display_msg import display
+from machine import Pin, ADC, Signal
+from time import sleep
 
-tester(display)
+# ESP32 witout spram
+btn = Pin(0, Pin.IN)
+def blink_fun(pin):
+    led.on() if btn() else led.off() 
+
+btn.irq(blink_fun)
+led = Signal(Pin(27, Pin.OUT, value=1), invert=False)
--- MicroWebSrv/workSpace/user_lib/alive_btn_test.py
+++ MicroWebSrv/workSpace/user_lib/alive_btn_test.py
@@ -1,10 +1,10 @@
 from machine import Pin, ADC, Signal
 from time import sleep
 
-# ESP32 witout spram
+# ESP32 witout spram 0=internal_btn, 1=internal_led
 btn = Pin(0, Pin.IN)
 def blink_fun(pin):
     led.on() if btn() else led.off() 
 
 btn.irq(blink_fun)
-led = Signal(Pin(1, Pin.OUT, value=1), invert=False)
+led = Signal(Pin(27, Pin.OUT, value=1), invert=False) # relay 3 click tset
