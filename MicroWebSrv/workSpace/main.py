bb4403776cad877280c28ed36fc045763b080145
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-31 23:28:26 +0300

    update

--- MicroWebSrv/workSpace/main.py
+++ MicroWebSrv/workSpace/main.py
@@ -1,11 +1,17 @@
 from machine import Pin
-from esp import flash_size
+# from esp import flash_size
+# import re
+# import micropython, gc
+import uos
 
-if flash_size() < 4200000:
+fs_stat = uos.statvfs('/')
+fs_size = fs_stat[0] * fs_stat[2]
+
+if fs_size == 2097152:
     # esp32 without spram - relay test
     relay = Pin(13, Pin.OUT, value=1)
     import user_lib.alive_btn_test
-else:
+elif fs_size == 2072576:
     # esp32 with spram - led will power off on reset
     led = Pin(0, Pin.OUT, Pin.PULL_UP)
 import ws
