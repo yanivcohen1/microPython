5320579ce4cae5589f4ed05d3eb67d3fd428139b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-31 18:04:20 +0300

    update

---
+++ MicroWebSrv/workSpace/user_lib/alive_btn_test.py
@@ -0,0 +1,10 @@
+from machine import Pin, ADC, Signal
+from time import sleep
+
+# ESP32 witout spram
+btn = Pin(0, Pin.IN)
+def blink_fun(pin):
+    led.on() if btn() else led.off() 
+
+btn.irq(blink_fun)
+led = Signal(Pin(1, Pin.OUT, value=1), invert=False)
