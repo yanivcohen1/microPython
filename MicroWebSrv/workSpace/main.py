d005620a46f3338a6efd97c95ffc9efa9ccea967
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-07-31 19:07:16 +0300

    update

--- MicroWebSrv/workSpace/main.py
+++ MicroWebSrv/workSpace/main.py
@@ -1,5 +1,11 @@
 from machine import Pin
-# led will power off on reset
-led = Pin(0, Pin.OUT, Pin.PULL_UP)
-# relay = Pin(13, Pin.OUT, value=1)
+from esp import flash_size
+
+if flash_size() < 4200000:
+    # esp32 without spram - relay test
+    relay = Pin(13, Pin.OUT, value=1)
+    import user_lib.alive_btn_test
+else:
+    # esp32 with spram - led will power off on reset
+    led = Pin(0, Pin.OUT, Pin.PULL_UP)
 import ws
