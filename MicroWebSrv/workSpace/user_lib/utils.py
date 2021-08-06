8dadb5b38e7c65daf0b72cc630d55f4fe62348ea
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-08-06 18:37:51 +0300

    ud

--- MicroWebSrv/workSpace/user_lib/utils.py
+++ MicroWebSrv/workSpace/user_lib/utils.py
@@ -1,4 +1,8 @@
 
 
 def mapInRangToOutRang(val_in, in_min, in_max, out_min, out_max):
-    return in_min + (((in_max - in_min)/(out_max - out_min))(val_in - out_min))
+    return out_min + (((out_max - out_min) / (in_max - in_min)) * (val_in - in_min))
+
+# test
+# print(mapInRangToOutRang(2,1,3,2,4)) # need to be 3
+# print(mapInRangToOutRang(2,1,3,2,6)) # need to be 4
