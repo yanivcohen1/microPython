db1d37d2679c5d611ea96fa8947879bade0f65f1
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-08-16 20:13:34 +0300

    add libs

--- MicroWebSrv/workSpace/test.py
+++ MicroWebSrv/workSpace/test.py
@@ -1,4 +1,4 @@
-# run tester - change potentiometer to change steper angle
-from user_lib.Stepper_uln2003 import tester
-from user_lib.display_msg import display
-tester(display)
+# run os list
+import os
+print("os List:")
+print (os.listdir())
