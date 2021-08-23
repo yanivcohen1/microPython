af2654e25dea5c53e893a1495dd1ac6b1a289f9a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-08-23 09:08:52 +0300

    add buzzer

--- MicroWebSrv/workSpace/user_lib/alive_btn_test.py
+++ MicroWebSrv/workSpace/user_lib/alive_btn_test.py
@@ -4,7 +4,7 @@ from time import sleep
 # ESP32 witout spram 0=internal_btn, 1=internal_led
 btn = Pin(0, Pin.IN)
 def blink_fun(pin):
-    led.on() if btn() else led.off() 
+    led.off() if btn() else led.on() 
 
 btn.irq(blink_fun)
-led = Signal(Pin(27, Pin.OUT, value=1), invert=False) # relay 3 click tset
+led = Signal(Pin(27, Pin.OUT, value=0), invert=False) # relay 3 click tset
