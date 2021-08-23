2d47eb91e207806c16b2fa6d8d7e9e9b1d58efec
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-08-23 09:11:22 +0300

    update

--- MicroWebSrv/workSpace/main.py
+++ MicroWebSrv/workSpace/main.py
@@ -9,11 +9,11 @@ import ubinascii
 device_unique_id = ubinascii.hexlify(machine.unique_id()).decode('utf-8')
 
 if device_unique_id == '2462abe768e4':
-    # esp32 without spram - relay test
+    # esp32 without spram - buzzer test
     relay = Pin(13, Pin.OUT, value=1)
     import user_lib.alive_btn_test
+    # import ws
 elif device_unique_id == '7c9ebd288188':
     # esp32 with spram - led will power off on reset
     led = Pin(0, Pin.OUT, Pin.PULL_UP)
-
-import ws
+    import ws
