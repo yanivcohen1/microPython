47cb0b18a93d4c4dd752cdbae08e3157d73a30fe
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2021-11-15 12:41:52 +0200

    update

--- MicroWebSrv/workSpace/user_lib/net.py
+++ MicroWebSrv/workSpace/user_lib/net.py
@@ -11,14 +11,14 @@ def wifi_connect():
     if not simulation:
         sta_if = network.WLAN(network.STA_IF)
         while not sta_if.isconnected():
-                print('connecting to network...')
-                try:
-                    sta_if.active(True)
-                    sta_if.connect("HOTBOX-89BA-yaniv", decrypt(b'C\xfcC\xe10>\xf8\xc4i\x88Da?\xd4\x82\x86'))
-                except:
-                    sleep(10)
-                # while not sta_if.isconnected():
-                    # pass
+            print('connecting to network...')
+            try:
+                sta_if.active(True)
+                sta_if.connect("HOTBOX-89BA-yaniv", decrypt(b'C\xfcC\xe10>\xf8\xc4i\x88Da?\xd4\x82\x86'))
+            except:
+                sleep(10)
+            # while not sta_if.isconnected():
+                # pass
         print('network config:', sta_if.ifconfig())
 
 # wifi_connect()
