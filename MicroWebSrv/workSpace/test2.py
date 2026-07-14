49e6b4de9e8a4751e2db907202d293157b055e8d
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2026-07-14 06:07:05 +0300

    Add ADC initialization for slider potentiometer in test2.py

--- MicroWebSrv/workSpace/test2.py
+++ MicroWebSrv/workSpace/test2.py
@@ -1,11 +1,14 @@
 import user_lib.settings as settings
 emulated = settings.isEmulated()
 if emulated:
-    from machine2 import Pin
+    from machine2 import Pin, ADC
 else:
-    from machine import Pin
+    from machine import Pin, ADC
 
 import time
+
+sliderPot = ADC(Pin(34))
+sliderPot.atten(ADC.ATTN_11DB) # Full range: 3.3v
 
 led = Pin(5, Pin.OUT)
 while True:
@@ -15,3 +18,5 @@ while True:
     led.off()
     time.sleep(1)
     print("status:", led.value())
+    
+    print("slider:", int(sliderPot.read() * 100 / 4095))
